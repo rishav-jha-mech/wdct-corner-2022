d3337daa89ff80ca765c3e328b0fc7f5f3ebca21
rishav-jha-mech <rishav.rj74@gmail.com>
2022-03-05 22:04:38 +0530

    Final Website with minor touches left

---
+++ main.js
@@ -0,0 +1,301 @@
+const arya = [
+
+    {
+        "img": '4Amit Kumar.jpg',
+        "name": 'Amit Kumar'
+    },
+    {
+        "img": 'Anushka Mandalpa2.jpeg',
+        "name": 'Anushka Mandal'
+    },
+    {
+        "img": 'Rajeev Yadav5.jpg',
+        "name": 'Rajeev Yadav'
+    },
+    {
+        "img": 'Satyaki Chaudhuri1.jpg',
+        "name": 'Satyaki Chaudhuri'
+    },
+    {
+        "img": 'ALAUKIK TIWARI.jpg',
+        "name": 'ALAUKIK TIWARI'
+    },
+    {
+        "img": 'Avinash Y.jpg',
+        "name": 'Avinash Y'
+    },
+    {
+        "img": 'Retuja Chatterjee.png',
+        "name": 'Retuja Chatterjee'
+    },
+    {
+        "img": 'Satyaki Chaudhuri2.jpg',
+        "name": 'Satyaki Chaudhuri'
+    },
+    {
+        "img": 'ALAUKIK TIWARI1.jpg',
+        "name": 'ALAUKIK TIWARI'
+    },
+    {
+        "img": 'Avinash Y2.jpg',
+        "name": 'Avinash Y'
+    },
+    {
+        "img": 'Retuja Chatterjee1.jpg',
+        "name": 'Retuja Chatterjee'
+    },
+    {
+        "img": 'Satyaki Chaudhuri3.jpg',
+        "name": 'Satyaki Chaudhuri'
+    },
+    {
+        "img": 'ALAUKIK TIWARI2.jpg',
+        "name": 'ALAUKIK TIWARI'
+    },
+    {
+        "img": 'Avinash Y3.jpg',
+        "name": 'Avinash Y'
+    },
+    {
+        "img": 'Retuja Chatterjee2.jpg',
+        "name": 'Retuja Chatterjee'
+    },
+    {
+        "img": 'Satyaki Chaudhuri4.jpg',
+        "name": 'Satyaki Chaudhuri'
+    },
+    {
+        "img": 'Aditya Mohapatra.jpg',
+        "name": 'Aditya Mohapatra'
+    },
+    {
+        "img": 'Avinash Y4.jpg',
+        "name": 'Avinash Y'
+    },
+    {
+        "img": 'Retuja Chatterjee3.jpg',
+        "name": 'Retuja Chatterjee'
+    },
+    {
+        "img": 'Shreya Maithani.jpg',
+        "name": 'Shreya Maithani'
+    },
+    {
+        "img": 'Aditya Mohapatra6.jpg',
+        "name": 'Aditya Mohapatra'
+    },
+    {
+        "img": 'Avinash Y5.jpg',
+        "name": 'Avinash Y'
+    },
+    {
+        "img": 'Retuja Chatterjee4.jpg',
+        "name": 'Retuja Chatterjee'
+    },
+    {
+        "img": 'Shreya Maithani1.jpg',
+        "name": 'Shreya Maithani'
+    },
+    {
+        "img": 'Amit Kumar.jpg',
+        "name": 'Amit Kumar'
+    },
+    {
+        "img": 'Flame 2 - Aditya Mohapatra.jpg',
+        "name": 'Aditya Mohapatra'
+    },
+    {
+        "img": 'Riddhi Chowdhury1.jpg',
+        "name": 'Riddhi Chowdhury'
+    },
+    {
+        "img": 'Shreya Maithani2.jpg',
+        "name": 'Shreya Maithani'
+    },
+    {
+        "img": 'Amit Kumar1.jpg',
+        "name": 'Amit Kumar'
+    },
+    {
+        "img": 'Kiran Bolagani.JPG',
+        "name": 'Kiran Bolagani'
+    },
+    {
+        "img": 'Riddhi Chowdhury2.png',
+        "name": 'Riddhi Chowdhury'
+    },
+    {
+        "img": 'Somwrik Dubey1.jpg',
+        "name": 'Somwrik Dubey'
+    },
+    {
+        "img": 'Aniket Kamble1.jpg',
+        "name": 'Aniket Kamble'
+    },
+    {
+        "img": 'Kiran Bolagani2.jpg',
+        "name": 'Kiran Bolagani'
+    },
+    {
+        "img": 'Riddhi Chowdhury3.jpg',
+        "name": 'Riddhi Chowdhury'
+    },
+    {
+        "img": 'Somwrik Dubey1.png',
+        "name": 'Somwrik Dubey'
+    },
+    {
+        "img": 'Aniket Kamble1.webp',
+        "name": 'Aniket Kamble'
+    },
+    {
+        "img": 'Kiran Bolagani3.jpeg',
+        "name": 'Kiran Bolagani'
+    },
+    {
+        "img": 'Riddhi Chowdhury4.jpg',
+        "name": 'Riddhi Chowdhury'
+    },
+    {
+        "img": 'Somwrik Dubey2.jpg',
+        "name": 'Somwrik Dubey'
+    },
+    {
+        "img": 'Aniket Kamble2.jpg',
+        "name": 'Aniket Kamble'
+    },
+    {
+        "img": 'Nilashis Mukherjee.jpg',
+        "name": 'Nilashis Mukherjee'
+    },
+    {
+        "img": 'Riddhi Chowdhury5.jpg',
+        "name": 'Riddhi Chowdhury'
+    },
+    {
+        "img": 'Sourav Mohanta.pdf',
+        "name": 'Sourav Mohanta'
+    },
+    {
+        "img": 'Aniket Kamble3.jpg',
+        "name": 'Aniket Kamble'
+    },
+    {
+        "img": 'Nilashis Mukherjee0.jpg',
+        "name": 'Nilashis Mukherjee'
+    },
+    {
+        "img": 'Rishi Marodia.jpg',
+        "name": 'Rishi Marodia'
+    },
+    {
+        "img": 'Sunrise - Aditya Mohapatra.jpg',
+        "name": 'Aditya Mohapatra'
+    },
+    {
+        "img": 'Anushka Mandal1.jpg',
+        "name": 'Anushka Mandal'
+    },
+    {
+        "img": 'Rajeev Yadav.jpg',
+        "name": 'Rajeev Yadav'
+    },
+    {
+        "img": 'Rishi Marodia1.jpg',
+        "name": 'Rishi Marodia'
+    },
+    {
+        "img": 'TAMANNA AILANI.jpeg',
+        "name": 'TAMANNA AILANI'
+    },
+    {
+        "img": 'Anushka Mandal2.jpg',
+        "name": 'Anushka Mandal'
+    },
+    {
+        "img": 'Rajeev Yadav1.jpg',
+        "name": 'Rajeev Yadav'
+    },
+    {
+        "img": 'Rishi Marodia3.jpg',
+        "name": 'Rishi Marodia'
+    },
+    {
+        "img": 'kiran Bolagani5.jpg',
+        "name": 'kiran Bolagani'
+    },
+    {
+        "img": 'Anushka Mandal3.jpeg',
+        "name": 'Anushka Mandal'
+    },
+    {
+        "img": 'Rajeev Yadav2.jpg',
+        "name": 'Rajeev Yadav'
+    },
+    {
+        "img": 'Rishi Marodia4.jpg',
+        "name": 'Rishi Marodia'
+    },
+    {
+        "img": 'meghna maddi.jpeg',
+        "name": 'meghna maddi'
+    },
+    {
+        "img": 'Anushka Mandalpa1.jpeg',
+        "name": 'Anushka Mandal'
+    },
+    {
+        "img": 'Rajeev Yadav3.jpg',
+        "name": 'Rajeev Yadav'
+    },
+    {
+        "img": 'Rishi Marodia6.jpg',
+        "name": 'Rishi Marodia'
+    },
+]
+
+const slide = document.getElementById('genuine_slide');
+const Thecontent = document.getElementById('genuine_content');
+
+
+
+arya.map((content, index) => {
+
+    console.log(content.img);
+    const divContent = document.createElement('div');
+    const divSlide = document.createElement('div');
+
+    const img_path = `${window.location.origin}/all_images/${content.img}`;
+    const name= content.name.toLowerCase()
+
+    divSlide.className = 'slide';
+    divContent.className = 'content__item';
+
+    divSlide.innerHTML = `
+        <div class="slide__img-wrap">
+            <div
+                class="slide__img"
+                style="background-image: url('${img_path}')">
+            </div>
+        </div>
+    <div class="slide__side">Memories &amp; Thoughts</div>
+    <div class="slide__title-wrap">
+        <span class="slide__number">${index + 1}</span>
+        <h3 class="slide__title">${content.name}</h3>
+        <h4 class="slide__subtitle"></h4>
+    </div>
+    `;
+
+    divContent.innerHTML = `
+    <span class="content__number">${index + 1}</span>
+    <h3 class="content__title">${content.name}</h3>
+    <h4 class="content__subtitle"></h4>
+    <div class="content__text">
+      <button class="btn" onclick="modaltoggle2('${img_path}')">Show Art</button>
+    </div>
+    `;
+
+    slide.prepend(divSlide);
+    Thecontent.prepend(divContent);
+
+})
